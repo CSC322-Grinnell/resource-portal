e7813174afa4c96ee4331583483e2baf951f3280
Zoe Wolter <wolterzo@grinnell.edu>
2015-11-12 00:22:44 +0000

    Preliminary set up for devise gem

--- resource-portal/config/routes.rb
+++ resource-portal/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -16,7 +17,7 @@ Rails.application.routes.draw do
  resources :resources
   # Example resource route with options:
   #   resources :products do
-  #     member do
+  #     member dog
   #       get 'short'
   #       post 'toggle'
   #     end
