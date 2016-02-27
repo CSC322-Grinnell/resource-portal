8bd7376a1baf2393c3f910b55cb50eba8a72d401
danicodes <williams11@grinnell.edu>
2016-02-27 22:38:20 +0000

    Add CanCan roles to user

--- resource-portal/app/models/user.rb
+++ resource-portal/app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  
+  
+  devise :database_authenticatable, :registerable, 
          :recoverable, :rememberable, :trackable, :validatable
+                              ## Multi-level admin
+  Roles = [ :admin, :default] ##Admin that can delete other admins, and admin that cannot
+  
+  def is?(requested_role)     
+    self.role == requested_role.to_s
+  end
 end
