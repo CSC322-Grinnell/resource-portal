900921b2374b1a77dfd4ccd61c8698c56ec0ac04
danicodes <williams11@grinnell.edu>
2016-02-27 22:38:20 +0000

    Add CanCan roles to user

--- resource-portal/app/models/user.rb
+++ resource-portal/app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :invitable, :database_authenticatable, :recoverable, :rememberable, 
          :trackable, :validatable
+                              ## Multi-level admin
+  Roles = [ :admin, :default] ##Admin that can delete other admins, and admin that cannot
+  
+  def is?(requested_role)     
+    self.role == requested_role.to_s
+  end
 end
--- resource-portal/db/schema.rb
+++ resource-portal/db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< 5a237ce32080286f91c2e392f049f275431d018e
 ActiveRecord::Schema.define(version: 20160427034802) do
+=======
+ActiveRecord::Schema.define(version: 20160225205345) do
+>>>>>>> Add CanCan roles to user
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -45,16 +49,19 @@ ActiveRecord::Schema.define(version: 20160427034802) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",        null: false
+    t.string   "encrypted_password",     default: "",        null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,         null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.datetime "created_at",                                 null: false
+    t.datetime "updated_at",                                 null: false
+    t.string   "role",                   default: "default"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "invitation_token"
