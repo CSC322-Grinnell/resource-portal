a2df96a7c54efd4322361f2ade3452525b9e9c2b
danicodes <williams11@grinnell.edu>
2016-02-27 22:38:20 +0000

    Add CanCan roles to user

--- resource-portal/db/schema.rb
+++ resource-portal/db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< 5a237ce32080286f91c2e392f049f275431d018e
 ActiveRecord::Schema.define(version: 20160427034802) do
-=======
-ActiveRecord::Schema.define(version: 20160225205345) do
->>>>>>> Add CanCan roles to user
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -59,25 +55,9 @@ ActiveRecord::Schema.define(version: 20160225205345) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "invitation_token"
-    t.datetime "invitation_created_at"
-    t.datetime "invitation_sent_at"
-    t.datetime "invitation_accepted_at"
-    t.integer  "invitation_limit"
-    t.integer  "invited_by_id"
-    t.string   "invited_by_type"
-    t.integer  "invitations_count",      default: 0
     t.datetime "created_at",                                 null: false
     t.datetime "updated_at",                                 null: false
     t.string   "role",                   default: "default"
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["invitation_token"], name: "index_users_on_invitation_token", unique: true
-  add_index "users", ["invitations_count"], name: "index_users_on_invitations_count"
-  add_index "users", ["invited_by_id"], name: "index_users_on_invited_by_id"
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
+  
 end
