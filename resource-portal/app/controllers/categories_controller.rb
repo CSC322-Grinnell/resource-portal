52b17a07a38cb20d26fb89d6adbd76b8e32aeeff
deweytyl <deweytyl@grinnell.edu>
2016-03-07 03:17:53 +0000

    Authenticate entire categories controller

--- resource-portal/app/controllers/categories_controller.rb
+++ resource-portal/app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!
 
   # GET /categories
   # GET /categories.json
