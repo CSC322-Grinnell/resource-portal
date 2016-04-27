b34c9431b25d65a0268fd5fd04ff91fb7c3a8800
deweytyl <deweytyl@grinnell.edu>
2016-04-27 01:13:13 +0000

    Add automatic resource approval for admins

--- resource-portal/app/controllers/resources_controller.rb
+++ resource-portal/app/controllers/resources_controller.rb
@@ -30,9 +30,18 @@ class ResourcesController < ApplicationController
   #Flashes on the view a note to indicate success of resouces creation
   #@return [void] but redirects to the ingidex.
   def create
-    resource = Resource.create!(resource_params)
-    flash[:success] = "#{resource.name} was successfully submitted."
-    redirect_to :root
+    @resource = Resource.new(resource_params)
+
+    # Only admins can be signed in users at this point
+    # If a user is signed in, then they are an admin
+    if user_signed_in?
+      @resource.status = "Approved"
+    end
+
+    if @resource.save
+      flash[:success] = "#{@resource.name} was successfully submitted."
+      redirect_to :root
+    end
   end
 
   def destroy
