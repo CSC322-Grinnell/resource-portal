f52fd2f72496d49e75a26f1482fd718b4debe27e
deweytyl <deweytyl@grinnell.edu>
2016-04-27 05:07:30 +0000

    Get basic ResourcesController tests to pass

--- resource-portal/test/controllers/resources_controller_test.rb
+++ resource-portal/test/controllers/resources_controller_test.rb
@@ -1,8 +1,12 @@
 require 'test_helper'
 
 class ResourcesControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+
   setup do
-    @resource = resources(:one)
+    @resource = resources(:mica)
+    @user = users(:one)
+    @category = categories(:employment)
   end
 
   test "should get index" do
@@ -18,10 +22,15 @@ class ResourcesControllerTest < ActionController::TestCase
 
   test "should create resource" do
     assert_difference('Resource.count') do
-      post :create, resource: {  }
+      post :create, resource: {
+        name: "a",
+        description_of_service: "b",
+        address: "c",
+        contact_email: "fake@email.com"
+      }, category_ids: [@category.id]
     end
 
-    assert_redirected_to resource_path(assigns(:resource))
+    assert_redirected_to :root
   end
 
   test "should show resource" do
@@ -30,20 +39,26 @@ class ResourcesControllerTest < ActionController::TestCase
   end
 
   test "should get edit" do
+    sign_in @user
+
     get :edit, id: @resource
     assert_response :success
   end
 
   test "should update resource" do
-    patch :update, id: @resource, resource: {  }
+    sign_in @user
+
+    patch :update, id: @resource, resource: { name: "new name" }
     assert_redirected_to resource_path(assigns(:resource))
   end
 
   test "should destroy resource" do
+    sign_in @user
+
     assert_difference('Resource.count', -1) do
       delete :destroy, id: @resource
     end
 
-    assert_redirected_to resources_path
+    assert_redirected_to admin_path
   end
 end
