ab30a017ce462a1b590d540cfc5aad721ec061d5
deweytyl <deweytyl@grinnell.edu>
2016-05-05 00:18:14 +0000

    Adding general and permission tests for CRUD routes.

--- resource-portal/test/controllers/resources_controller_test.rb
+++ resource-portal/test/controllers/resources_controller_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class ResourcesControllerTest < ActionController::TestCase
-  include Devise::TestHelpers
+
+  def resource_params
+    { resource: {
+        name: "a",
+        description_of_service: "b",
+        address: "c",
+        contact_email: "fake@email.com"
+      }, category_ids: [@category.id]
+    }
+  end
 
   setup do
     @resource = resources(:mica)
@@ -9,25 +18,21 @@ class ResourcesControllerTest < ActionController::TestCase
     @category = categories(:employment)
   end
 
-  test "should get index" do
+  test "should get index, regardless of login credentials" do
     get :index
     assert_response :success
     assert_not_nil assigns(:resources)
   end
 
-  test "should get new" do
+  test "should get new resource form, regardless of login credentials" do
     get :new
     assert_response :success
   end
 
-  test "should create resource" do
+  test "should create resource that is then sent to the pending status" do
+    #sign_in @user?
     assert_difference('Resource.count') do
-      post :create, resource: {
-        name: "a",
-        description_of_service: "b",
-        address: "c",
-        contact_email: "fake@email.com"
-      }, category_ids: [@category.id]
+      post :create, resource_params
     end
 
     assert_redirected_to :root
@@ -38,18 +43,28 @@ class ResourcesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get edit" do
+  test "logged in user should be able to access edit resource form" do
     sign_in @user
-
     get :edit, id: @resource
     assert_response :success
   end
 
+
+  test "logged out user should not be able to access edit resource form" do
+    get :edit, id: @resource
+    assert_redirected_to new_user_session_path
+  end
+
   test "should update resource" do
     sign_in @user
+    new_name = "new name"
 
-    patch :update, id: @resource, resource: { name: "new name" }
-    assert_redirected_to resource_path(assigns(:resource))
+    patch :update, id: @resource, resource: { name: new_name }
+
+    @resource.reload
+
+    assert_equal new_name, @resource.name, "Did not update resource"
+    assert_redirected_to admin_path
   end
 
   test "should destroy resource" do
@@ -60,5 +75,17 @@ class ResourcesControllerTest < ActionController::TestCase
     end
 
     assert_redirected_to admin_path
+  end
+
+
+  test "Automatically submit resource if admin is logged in" do
+    sign_in @user
+
+    get :new # Access the new form page
+    assert_response :success # Make sure the new page responded
+    assert_difference('Resource.count') do # Make sure the resource was created
+      post :create, resource_params
+    end
+    assert_redirected_to :admin # After successful creation, redirected to admin page.
   end
 end
--- resource-portal/test/controllers/users_controller_test.rb
+++ resource-portal/test/controllers/users_controller_test.rb
@@ -1,49 +1,76 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+
   setup do
     @user = users(:one)
   end
 
-  test "should get index" do
+  # Index tests
+  test "should get index of users if signed in" do
+    sign_in @user
     get :index
     assert_response :success
     assert_not_nil assigns(:users)
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
+  test "should not get index of users if not signed in" do
+    get :index
+    assert_redirected_to new_user_session_path
   end
 
-  test "should create user" do
-    assert_difference('User.count') do
-      post :create, user: {  }
-    end
-
-    assert_redirected_to user_path(assigns(:user))
-  end
-
-  test "should show user" do
+  # Show tests
+  test "should show user if signed in" do
+    sign_in @user
     get :show, id: @user
     assert_response :success
   end
 
-  test "should get edit" do
-    get :edit, id: @user
-    assert_response :success
+  test "should not show user if not signed in" do
+    get :show, id: @user
+    assert_redirected_to new_user_session_path
   end
 
-  test "should update user" do
-    patch :update, id: @user, user: {  }
-    assert_redirected_to user_path(assigns(:user))
-  end
-
-  test "should destroy user" do
+  # Destroy Test
+  test "should destroy user if logged in" do
+    sign_in @user
     assert_difference('User.count', -1) do
       delete :destroy, id: @user
     end
-
     assert_redirected_to users_path
   end
+
+  # No user redirect test
+  test "signed out user is redirected to admin login if attempts to admin page page" do
+    get :admin
+    assert_redirected_to new_user_session_path
+    #assert_response :failure
+  end
+
+# Mail invite page access tests
+#  test "able to access mail invite page if not logged in" do
+#    sign_in @user
+#    get new_user_invitation_path
+#    assert_response :success
+#  end
+
+#  test "unable to access mail invite page if not logged in" do
+#    get new_user_invitation_path
+#    assert_redirected_to new_user_session_path
+#  end
+
+  # When a user attempts to send out an invite to an invalid address,
+  # they get redirected to a new invite page. This test ensures that a
+  # one must be signed in to access it.
+  test "unable to access failed invitation invite page if not logged in" do
+
+  end
+
+  test "" do
+
+  end
+
+  # Tests for mail user invitation accept?
+
+
 end
