33941fc86e7798006c47697334e8fde648e1521f
Sarah Trop <sctrop@gmail.com>
2016-10-28 18:31:25 +0000

    Successful redirect to root when default user signs in

--- resource-portal/app/models/ability.rb
+++ resource-portal/app/models/ability.rb
@@ -2,8 +2,8 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new
-    if user.is? :admin
+    user ||= User.new 
+    if user.role == :admin
        can :manage, :all
     else
        can :read, Resource
