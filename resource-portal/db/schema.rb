61e21c0c7991da10c1219bccb1d66e1edd56300d
Sarah Trop <sctrop@gmail.com>
2016-11-15 21:08:23 +0000

    Static Google Maps integration

--- resource-portal/db/schema.rb
+++ resource-portal/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161114191143) do
+ActiveRecord::Schema.define(version: 20161115210144) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -49,6 +49,8 @@ ActiveRecord::Schema.define(version: 20161114191143) do
     t.string   "reviewer_name"
     t.time     "date_reviewed"
     t.text     "reviewer_comment"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   create_table "taggings", force: :cascade do |t|
