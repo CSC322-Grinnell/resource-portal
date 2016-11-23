4e9aa207184dcd31b806b1cde353e3a1b1e57254
Sarah Trop <sctrop@gmail.com>
2016-11-23 20:06:44 +0000

    Working on javascript

--- resource-portal/app/assets/javascripts/resource.js
+++ resource-portal/app/assets/javascripts/resource.js
@@ -1,13 +1,33 @@
 $(document).ready(function() {
+    /*
+    
+    // OPTION 1
+    $('.category-box-container input:radio[type=radio_button]').change( function(){
+        var c = $(".category-box-container input[type=check_box]:checked").val();
+        console.log("HELLO");
+        console.log(c);
+        if($('.' + c + '_selection').is(":checked"))   
+            $('.' + c + '_tag').show();
+        else
+            $('.' + c + '_tag').hide();
+    });
+    
+    // OPTION 2
+    $('.category-box-container input:radio[type=radio_button]').change( function() {
+        $('.category-box-container input[id=' + this.value + ']').toggle(this.checked);
+    }).change();
+    
+    // OPTION 3
+    
+    $(function(){
+    var $tags = $('.' + c + '_tag').parent();
+    $tags.hide();
 
-$('.category-box-container input:radio[type=radio_button]').change( function(){
-    var c = $(".category-box-container input[type=radio_button]:checked").val();
-    console.log("HELLO");
-    console.log(c);
-    if($('.' + c + '_selection').is(":checked"))   
-        $('.' + c + '_tag').show();
-    else
-        $('.' + c + '_tag').hide();
-  });
-
-});
+    $('.category-box-container').on('click',$('.' + c + '_selection'),function(){
+        if ( $(this).is(':checked') ) {
+            $tags.show();
+        } else{
+            $tags.hide();
+        }
+    }); */
+}); 
