630260c074b25218a09a75448ab78f92c8a60567
Sarah Trop <sctrop@gmail.com>
2016-11-28 04:10:39 +0000

    Fixing jQuery

--- resource-portal/app/assets/javascripts/resource.js
+++ resource-portal/app/assets/javascripts/resource.js
@@ -1,13 +1,11 @@
 /*global $*/
 // Code that shows and hides tag checkboxes based on the category selected
 $(document).ready(function() {
-    $('#' + $('input:radio:checked').attr('id') + '_tags').show();
     // On the change for the radio button
     $('input:radio').change(function(){
         // Hide all tag checkboxes
         $('.all_tags').hide();
         // Get the id of the selected category radio button and show that container
-        console.log($('input:radio:checked').attr('id'));
         $('#' + $('input:radio:checked').attr('id') + '_tags').show();
     });
 });
