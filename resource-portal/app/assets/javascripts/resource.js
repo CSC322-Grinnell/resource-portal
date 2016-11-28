18b91783c68b475f03566040e0a94965d123dbd1
Sarah Trop <sctrop@gmail.com>
2016-11-28 00:20:36 +0000

    Form finished with jQuery hiding and showing of tags

--- resource-portal/app/assets/javascripts/resource.js
+++ resource-portal/app/assets/javascripts/resource.js
@@ -1,19 +1,12 @@
 /*global $*/
+// Code that shows and hides tag checkboxes based on the category selected
 $(document).ready(function() {
-
+    $('#' + $('input:radio:checked').attr('id') + '_tags').show();
     // On the change for the radio button
     $('input:radio').change(function(){
-        // Get the id of the container of that radio button
-        var c = $('input:radio:checked').attr('id');
-        // Loop through the tag checkboxes and show the ones that match the radio button
-        $('input:checkbox').each(function(index, myCheckbox) {
-            var tagName = c + '_tag';
-            // If the checkbox has the right id
-            if($(myCheckbox).attr('id') == tagName) {
-                console.log("show " + $(myCheckbox).val());
-                // Show the checkbox and its label
-                $('#' + tagName).parent().show();
-            }
-        });
+        // Hide all tag checkboxes
+        $('.all_tags').hide();
+        // Get the id of the selected category radio button and show that container
+        $('#' + $('input:radio:checked').attr('id') + '_tags').show();
     });
-}); 
+});
