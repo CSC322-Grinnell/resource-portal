8dd56b6e89a413cf555328a704860c158b1305b2
Ruth Wu <wuruth17@grinnell.edu>
2016-11-30 19:35:31 +0000

    iangit add .

--- resource-portal/app/controllers/search_controller.rb
+++ resource-portal/app/controllers/search_controller.rb
@@ -7,5 +7,21 @@ class SearchController < ApplicationController
     @results = @resources #+ @categories
     # may be necessary to remove duplicates
   end 
+  
+  def categories
+    @categories = Category.search(params[:q])#:match :word_start#, suggest :true
+
+    # combine the results
+    @results = @categories
+    # may be necessary to remove duplicates
+  end 
+  
+  def tags
+    @tags = Tag.search(params[:q])#:match :word_start#, suggest :true
+
+    # combine the results
+    @results = @tags
+    # may be necessary to remove duplicates
+  end 
 
 end
--- resource-portal/db/schema.rb
+++ resource-portal/db/schema.rb
@@ -81,18 +81,18 @@ ActiveRecord::Schema.define(version: 20161121194558) do
   add_index "tags", ["name"], name: "index_tags_on_name", unique: true
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",        null: false
+    t.string   "encrypted_password",     default: "",        null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,         null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                                 null: false
+    t.datetime "updated_at",                                 null: false
     t.string   "invitation_token"
     t.datetime "invitation_created_at"
     t.datetime "invitation_sent_at"
@@ -101,7 +101,7 @@ ActiveRecord::Schema.define(version: 20161121194558) do
     t.integer  "invited_by_id"
     t.string   "invited_by_type"
     t.integer  "invitations_count",      default: 0
-    t.string   "role"
+    t.string   "role",                   default: "default"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
