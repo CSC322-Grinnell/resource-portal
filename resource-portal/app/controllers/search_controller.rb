705be403b17019ae579db55ff978d7aff23b9241
Sarah Trop <sctrop@gmail.com>
2016-12-09 17:20:37 +0000

    Ruth's additional search for categories

--- resource-portal/app/controllers/search_controller.rb
+++ resource-portal/app/controllers/search_controller.rb
@@ -2,9 +2,10 @@ class SearchController < ApplicationController
 
   def resources 
     @resources = Resource.search(params[:q])#:match :word_start#, suggest :true
-
+    @categories = Category.search(params[:q])#:match :word_start#, suggest :true
+    
     # combine the results
-    @results = @resources #+ @categories
+    @results = @resources + @categories
     # may be necessary to remove duplicates
   end 
 
